73b7b656c59569829ec2523d317a5d84c2cb98ee
John Robert Kanu <j.robert.kanu@gmail.com>
2019-10-09 08:14:51 -0400

    sketched psedudocode for sum_or_product

--- Excercises/RB101-RB109/Easy-2/sum_or_product.rb
+++ Excercises/RB101-RB109/Easy-2/sum_or_product.rb
@@ -2,3 +2,21 @@
 # asks if the user wants to determine the sum or product of all numbers between
 # 1 and the entered integer.
 
+# get int and arg
+# validate input
+# - loop
+# -- break if input matches regex \d+[,\s]?[p|s]
+# -- else ask again
+# - end
+# parse input
+# - grab int with //d/ regex
+# - grab arg with //w/ regex
+# - return as array
+# compute result given array [int, arg]
+# - if arg == s
+# -- calc sum
+# - if arg == p
+# -- calc product
+# print result
+#
+# note: using for loop to populate array works, but using .each does not. Why?
