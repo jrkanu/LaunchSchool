11d3383aebed99366d4b1eda79521d0bcf824c7b
John Robert Kanu <j.robert.kanu@gmail.com>
2019-10-09 07:59:40 -0400

    Started sum or product

---
+++ Excercises/RB101-RB109/Easy-2/sum_or_product.rb
@@ -0,0 +1,4 @@
+# Write a program that asks the user to enter an integer greater than 0, then
+# asks if the user wants to determine the sum or product of all numbers between
+# 1 and the entered integer.
+
