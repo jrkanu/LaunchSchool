ecca5fcd620c66aeb4581a22645dd6634aec1d34
John Robert Kanu <j.robert.kanu@gmail.com>
2019-10-11 18:54:47 -0400

    Started RPS assignment. Wrote spec.

---
+++ backend/rb101/lesson-2/rock_paper_scissors/rps.rb
@@ -0,0 +1,15 @@
+# Create a Rock, Paper Scissors game with the following features:
+#
+# 1. Lizard, Spock options.
+#     Rules: ('-->' = 'beats')
+#     ------------------------------
+#     Rock     -->  Lizard, Scissors
+#     Paper    -->  Spock, Rock
+#     Scissors -->  Paper, Lizard
+#     Spock    -->  Rock, Scissors
+#     Lizard   -->  Spock, Paper
+#     ------------------------------
+#
+# 2. Abbreviated input options (e.g., 'r' instead of 'rock')
+#
+# 3. Score keeper. First to five wins the round.
