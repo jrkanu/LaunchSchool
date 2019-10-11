71142f8de0b8b93d93be53c1429241ad0a5ff51e
John Robert Kanu <j.robert.kanu@gmail.com>
2019-10-11 19:49:54 -0400

    Fleshed out pseudocode a bit more, defined ruleset constants.

--- backend/rb101/lesson-2/rock_paper_scissors/rps.rb
+++ backend/rb101/lesson-2/rock_paper_scissors/rps.rb
@@ -13,3 +13,43 @@
 # 2. Abbreviated input options (e.g., 'r' instead of 'rock')
 #
 # 3. Score keeper. First to five wins the round.
+#
+# Establish constants, CLASSIC_RULES and KASS_RULES
+# Ask user if they want to play classic RPS or Kass version.
+# - set_ruleset(choice)
+# -- if choice == 'classic'
+# --- ruleset = CLASSIC_RULES
+# -- elsif choice == 'kass'
+# --- ruleset == KASS_RULES
+# -- end
+# SET score = {player: 0, computer: 0}
+# Ask user for their play, SET = user_choice
+# Let computer play, SET = ai_choice
+# Display user and computer choices.
+# Determine who won based on ruleset
+# - if ruleset == 'classic'
+# -- if user_choice == ai_choice
+# --- "It's a tie!"
+# -- elsif CLASSIC_RULES[user_choice].include?(ai_choice)
+# --- "Player wins!"
+# --- score[:player] += 1
+# -- else
+# --- "Computer wins!"
+# --- score[:computer] += 1
+# -- end
+
+CLASSIC_RULES = {
+  :valid     => %w(rock paper scissors),
+  'rock'     => %w(scissors),
+  'paper'    => %w(rock),
+  'scissors' => %w(paper)
+}
+
+KASS_RULES = {
+  :valid     => %w(rock paper scissors lizard spock),
+  'rock'     => %w(lizard scissors),
+  'paper'    => %w(spock rock),
+  'scissors' => %w(paper lizard),
+  'spock'    => %w(rock scissors),
+  'lizard'   => %w(spock paper)
+}
